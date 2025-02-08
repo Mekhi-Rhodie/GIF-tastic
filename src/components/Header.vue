6bf9cb2ffb91700f2d724377885db8f7e957ff96
Mekhi Rhodie <mekhirhodie@gmail.com>
2025-02-07 22:18:44 -0500

    Update Header.vue
    - add linear gradient to header back ground
    - add button for authentication modal toggle

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,27 @@
 <template>
-    
+    <header class="container">
+        <span class="row">
+            <h1 class="display-1">GIF-tastic!</h1>
+            <span class="auth-button-group">
+                <button class="auth-btn btn btn-primary">Login</button>
+                <button class="auth-btn btn btn-secondary">Register</button>
+            </span>
+        </span>
+    </header>
 </template>
 
 <style scoped>
-
+    header {
+        background-image: linear-gradient(to left,#00E682, #0D6EFD, #7B11D8);
+    }
 </style>
 
 <script>
+    export default {
+        data () {
+            return {
 
+            }
+        }
+    }
 </script>
