fc8448475db9f02746bfa3a213a8430c0242176f
Mekhi Rhodie <mekhirhodie@gmail.com>
2025-02-07 22:17:33 -0500

    Update App.vue
    - render 'Header' component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
-<script setup>
+<script>
+import Header from './components/Header.vue';
+
+export default {
+  components: {
+    Header
+  }
+}
 
 </script>
 
 <template>
- 
+  <Header />
 </template>
 
 <style scoped>
-
+</style>
